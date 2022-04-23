7cde36657105a449045186921069ce7e785c5333
daisen-you <one.more.pc.you.kob@gmail.com>
2022-04-23 20:42:23 +0900

    テストコードを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,10 @@ class User < ApplicationRecord
   has_many :items
   has_many :purchases
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
-  validates :password, presence: true, length: {minimum: 6 }
-  validates :password, confirmation: true
   validates :birth_day, presence: true
-  validates :email, presence: true, uniqueness: true
-  
+
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :nickname
     validates :family_name
@@ -22,7 +19,6 @@ class User < ApplicationRecord
 
   with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナ文字を使用してください' } do
     validates :family_name_kana
-    validates :first_name_kana    
+    validates :first_name_kana
   end
 end
-
