987b837aff1152f11877897892dfbedbf9584b7c
daisen-you <one.more.pc.you.kob@gmail.com>
2022-04-24 19:28:11 +0900

    修正　４回目

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: '英字と数字の両方を含めて設定してください'
-  validates :password, presence: true, length: {minimum: 6 }
   validates :birth_day, :nickname, presence: true
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
