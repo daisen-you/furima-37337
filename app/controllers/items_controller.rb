f048384a771e3dc0a16e8550447fd0b02922e945
daisen-you <one.more.pc.you.kob@gmail.com>
2022-05-22 11:54:05 +0900

    修正２回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if @item.user_id == current_user.id && @item.purchase.nil?
+    if @item.user_id == current_user.id || @item.purchase.present?
     else
       redirect_to root_path
     end
